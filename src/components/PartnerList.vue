33ebabcc918812433698a96022c68f4a3849b760
Caleb Cox <canac@users.noreply.github.com>
2020-12-30 19:16:08 -0600

    Use the calendar component

--- src/components/PartnerList.vue
+++ src/components/PartnerList.vue
@@ -1,110 +1,63 @@
 <template>
   <div class="partner-list">
-    <div class="day" v-for="day in visibleDays" :key="day.id">
-      <div class="day-name">
-        {{ formatDay(day.day) }}
-        <button @click="completeDay(day)" v-if="!day.completed && day.partners.length > 0">Complete</button>
-      </div>
-      <div class="partners">
-        <div
-          class="partner"
-          :class="{ complete: day.completed, incomplete: !day.completed }"
-          v-for="partner in day.partners"
-          :key="partner"
-        >
+    <MonthCalendar :month="activeMonth" v-slot:default="{ day }">
+      <div class="day" :class="isDayCompleted(day) ? 'complete' : 'incomplete'">
+        <button
+          @click="completeDay(day)"
+          v-if="!isDayCompleted(day) && getDayPartners(day).length > 0">
+          Complete
+        </button>
+        <div class="partner" v-for="partner in getDayPartners(day)" :key="partner">
           {{ partner }}
         </div>
       </div>
-    </div>
+    </MonthCalendar>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  addDays, getDate, eachDayOfInterval, eachMonthOfInterval,
-  endOfMonth, format, isBefore, isWeekend, startOfDay, startOfMonth, subDays,
-} from 'date-fns';
+import { isBefore, startOfDay, subDays } from 'date-fns';
 import PartnerService from '../services/PartnerService';
-import { Vue } from 'vue-class-component';
+import MonthCalendar from './MonthCalendar.vue';
+import { Options, Vue } from 'vue-class-component';
 
-type DayInfo = {
-  id: number;
-  partners: string[];
-  day: Date;
-  completed: boolean;
-};
+const partnerService: PartnerService = new PartnerService();
 
+@Options({
+  components: {
+    MonthCalendar,
+  },
+})
 export default class PartnerList extends Vue {
+  activeMonth: Date = new Date();
+
   lastCompletedDay: Date = subDays(startOfDay(new Date()), 3);
-  numVisibleDays: number = 10;
 
-  partnerService: PartnerService = new PartnerService();
-
-  calculatePartnersInRange(start: Date, end: Date) {
-    return eachDayOfInterval({ start, end }).map(day => this.partnerService.getPartnersForDay(day));
+  isDayCompleted(day: Date): boolean {
+    return !isBefore(this.lastCompletedDay, day);
   }
 
-  get visibleDays(): DayInfo[] {
-    return eachDayOfInterval({
-      start: subDays(startOfDay(new Date()), this.numVisibleDays),
-      end: startOfDay(new Date())
-    }).map((day, index): DayInfo => {
-      return {
-        id: index,
-        partners: this.partnerService.getPartnersForDay(day),
-        day,
-        completed: !isBefore(this.lastCompletedDay, day),
-      };
-    });
+  getDayPartners(day: Date): string[] {
+    return partnerService.getPartnersForDay(day);
   }
 
-  formatDay(day: Date) {
-    return format(day, 'EEEE, MMMM d');
-  }
-
-  completeDay(day: DayInfo) {
-    this.lastCompletedDay = day.day;
+  completeDay(day: Date) {
+    this.lastCompletedDay = day;
   }
 };
 </script>
 
 <style scoped>
-.partner-list {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
 .day {
-  border-bottom: 1px solid black;
-  margin-bottom: 3em;
-}
-
-.day-name {
-  color: gray;
-  font-size: 1.3em;
-  padding-bottom: 0.5em;
   text-align: center;
+  padding: 3em 0;
+  margin: 1px;
 }
 
-.partners {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.partner {
-  width: 10em;
-  height: 3em;
-  margin: 0.3em;
-  background-color: #dddddd;
-  font-size: 1.6em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.partner.incomplete {
+.day.incomplete {
   background-color: hsl(0, 50%, 80%);
 }
-.partner.complete {
+.day.complete {
   background-color: hsl(120, 50%, 80%);
 }
 </style>
