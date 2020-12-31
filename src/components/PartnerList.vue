db52a7494bdf890536e4ff617e254594c4240dda
Caleb Cox <canac@users.noreply.github.com>
2020-12-31 13:15:10 -0600

    Format button header

--- src/components/PartnerList.vue
+++ src/components/PartnerList.vue
@@ -2,11 +2,30 @@
   <div class="partner-list">
     <MonthCalendar :month="activeMonth" v-slot:default="{ day }">
       <div class="day" :class="isDayCompleted(day) ? 'complete' : 'incomplete'">
-        <button
-          @click="completeDay(day)"
-          v-if="!isDayCompleted(day) && getDayPartners(day).length > 0">
-          Complete
-        </button>
+        <div class="day-header">
+          <div class="button-slot">
+            <button
+              class="toggle-active"
+              @click="toggleDayActive(day)">
+              <i class="fas" :class="isDayActive(day) ? 'fa-eye-slash' : 'fa-eye'" />
+            </button>
+          </div>
+          <span class="day-title">{{ day.getDate() }}</span>
+          <div class="button-slot">
+            <button
+              class="mark-complete"
+              @click="completeDay(day)"
+              v-if="!isDayCompleted(day) && getDayPartners(day).length > 0">
+              <i class="fas fa-check" />
+            </button>
+            <button
+              class="mark-incomplete"
+              @click="uncompleteDay(day)"
+              v-if="isDayCompleted(day) && getDayPartners(day).length > 0">
+              <i class="fas fa-trash" />
+            </button>
+          </div>
+        </div>
         <div class="partner" v-for="partner in getDayPartners(day)" :key="partner">
           {{ partner }}
         </div>
@@ -16,7 +35,10 @@
 </template>
 
 <script lang="ts">
-import { isBefore, startOfDay, subDays } from 'date-fns';
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+
+import { isBefore, isWeekend, startOfDay, subDays } from 'date-fns';
 import PartnerService from '../services/PartnerService';
 import MonthCalendar from './MonthCalendar.vue';
 import { Options, Vue } from 'vue-class-component';
@@ -37,19 +59,62 @@ export default class PartnerList extends Vue {
     return !isBefore(this.lastCompletedDay, day);
   }
 
+  isDayActive(day: Date): boolean {
+    return !isWeekend(day);
+  }
+
   getDayPartners(day: Date): string[] {
     return partnerService.getPartnersForDay(day);
   }
 
+  toggleDayActive(day: Date) {
+  }
+
   completeDay(day: Date) {
     this.lastCompletedDay = day;
+  }
+
+  uncompleteDay(day: Date) {
+    this.lastCompletedDay = subDays(day, 1);
   }
 };
 </script>
 
 <style scoped>
 .day {
-  padding: 1em;
+  padding: 0.5em 1em;
+}
+
+.day-header {
+  padding-bottom: 0.25em;
+  display: flex;
+  font-size: 1.3em;
+}
+
+.day-header .button-slot {
+  width: 1.7em;
+}
+
+.day-header button {
+  border: none;
+  background-color: transparent;
+  font-size: inherit;
+}
+
+.day-header .mark-complete {
+  color: hsl(120, 50%, 50%);
+}
+.day-header .mark-incomplete {
+  color: hsl(0, 50%, 50%);
+}
+.day-header .toggle-active {
+  color: #999999;
+}
+
+.day-header .day-title {
+  flex: 1;
+  color: #666666;
+  text-align: center;
 }
 
 .partner {
